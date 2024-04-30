ba1789df3d5c53794f1506cc86fec566d810a958
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2024-04-30 00:10:04 -0400

    chore: update app version to 0.1.1

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import projectCreation from "./project-creation.js";
 const main = () => {
   const program = new Command();
   program
-    .version("1.0.0", "-v, --version", "Output the current version")
+    .version("0.1.1", "-v, --version", "Output the current version")
     .parse(process.argv);
 
   logger.info(
